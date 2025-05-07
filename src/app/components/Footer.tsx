0d163523f4b5c3bd8a91d54b641a00257fdcfaa9
RayanaVital <rayanavital@gmail.com>
2025-05-07 16:33:31 -0300

    fix: update footer text to include the correct author name

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="text-center text-sm py-6 text-gray-500 bg-pink-50">
-        © {new Date().getFullYear()} - Feito com ❤️ por Você
+        © {new Date().getFullYear()} - Feito com ❤️ por Rayana Vital
       </footer>
     )
   }
